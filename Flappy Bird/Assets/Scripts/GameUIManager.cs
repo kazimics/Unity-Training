3f47ae20551d599e98c6ac193084a1d7fffdb59f
Kazimi <a1205565748@gmail.com>
2020-08-29 18:50:02 +0800

    添加 Flappy Bird 项目

---
+++ Flappy Bird/Assets/Scripts/GameUIManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Image readyImage;
+
+    private void Start()
+    {
+        readyImage = GameObject.Find("Ready_Image").GetComponent<Image>();
+    }
+
+    public void readyTap()
+    {
+        readyImage.enabled = false;
+    }
+}
---
+++ Flappy Bird/Assets/Scripts/Tap.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tap : MonoBehaviour
+{
+    [SerializeField]
+    private Animator birdAnim;
+    private bool isFirstTap = true;
+    [SerializeField]
+    private GameUIManager uIManager;
+
+    private void Start()
+    {
+        birdAnim = GameObject.Find("Bird").GetComponent<Animator>();
+        uIManager = GameObject.Find("Canvas").GetComponent<GameUIManager>();
+    }
+
+    private void OnMouseDown()
+    {
+        if (isFirstTap == true)
+        {
+            uIManager.readyTap();
+            birdAnim.SetBool("isReady", true);
+            birdAnim.applyRootMotion = true;
+            isFirstTap = false;
+        }
+
+    }
+}
